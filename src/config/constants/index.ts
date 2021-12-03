50fbaf58313d2cb138dbbce75fb6a9645a32b3fb
zhangpengqiang <zpq0724@163.com>
2021-12-03 18:20:44 +0800

    feat: 修改兑换，增加流动性合约配置文件

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@pancakeswap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0x96FD300e3E4DAb1fD9EBDb47274Fd10D67B50B57'
 
 // a list of tokens by chain
 type ChainTokenList = {
--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -87,9 +87,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
-  '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', // v2 router 02
+  '0xE1723B18172ad29de6475afa63F3DF330868F09C', // v2 factory
+  '0xA110327F59b7fC9C817d4c02115dFEd160e8d032', // v2 router 01
+  '0x96FD300e3E4DAb1fD9EBDb47274Fd10D67B50B57', // v2 router 02
 ]
 
 /**
