9020726114b5aabdea11714ef88cf5f3da1624fc
wentai.zhou <943990462@qq.com>
2021-12-07 16:55:15 +0800

    Feature # 该了暗色主题的UserDetail背景

--- src/views/Home/components/UserBanner/index.tsx
+++ src/views/Home/components/UserBanner/index.tsx
@@ -9,7 +9,10 @@ const StyledCard = styled(Box)`
   border-left: 1px solid #D53B79;
   border-right: 1px solid #D53B79 ;
   border-radius: ${({ theme }) => `0 0 ${theme.radii.card} ${theme.radii.card}`};
-  background:  url(/images/mImg/blg.png);
+  background: ${({ theme }) =>
+  theme.isDark
+    ? 'linear-gradient(360deg, rgba(49, 61, 92, 0.9) 0%, rgba(61, 42, 84, 0.9) 100%)'
+    : 'linear-gradient(180deg, rgba(202, 194, 236, 0.9) 0%,  rgba(204, 220, 239, 0.9) 51.04%, rgba(206, 236, 243, 0.9) 100%)'};
 `
 
 const UserBanner = () => {
